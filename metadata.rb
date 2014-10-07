f13997a07514f108db6cb08513d154693743cc1b
Xavier Jodoin <xavier@jodoin.me>
2014-10-07 13:17:56 -0400

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -8,6 +8,7 @@ description      'Installs and configures a Kafka broker'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.6.0'
 
+recipe 'kafka::default', 'Installs Kafka'
 recipe 'kafka::source', 'Downloads, compiles and installs Kafka from source releases'
 recipe 'kafka::binary', 'Downloads, extracts and installs Kafka from binary releases'
 
